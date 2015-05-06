f539e8b81adbad052c9fc1ed3473b8759f397883
Brandon Moore <brandon.3.moore@gmail.com>
2015-05-06 15:39:54 -0500

    Connect new kast unparser to option

--- kernel/src/main/java/org/kframework/backend/unparser/PrintTerm.java
+++ kernel/src/main/java/org/kframework/backend/unparser/PrintTerm.java
@@ -4,29 +4,40 @@ package org.kframework.backend.unparser;
 import org.kframework.kil.ASTNode;
 import org.kframework.kil.Attributes;
 import org.kframework.kil.loader.Context;
+import org.kframework.kore.K;
+import org.kframework.kore.ToKast;
+import org.kframework.kore.convertors.KILtoInnerKORE;
 import org.kframework.krun.ColorOptions;
 import org.kframework.transformation.Transformation;
 
 import com.google.inject.Inject;
+import org.kframework.utils.inject.Main;
 
 public class PrintTerm implements Transformation<ASTNode, String> {
 
     private final ColorOptions colorOptions;
     private final OutputModes mode;
+    private final Context context;
 
     @Inject
     public PrintTerm(
             ColorOptions colorOptions,
-            OutputModes mode) {
+            OutputModes mode,
+            @Main Context context) {
         this.colorOptions = colorOptions;
         this.mode = mode;
+        this.context = context;
     }
 
     @Override
     public String run(ASTNode node, Attributes a) {
-        return new Unparser(a.typeSafeGet(Context.class),
-                colorOptions.color(), colorOptions.terminalColor(),
-                mode != OutputModes.NO_WRAP, false).print(node);
+        if (mode == OutputModes.KAST) {
+            return ToKast.apply((K) new KILtoInnerKORE(context, false).apply(node));
+        } else {
+            return new Unparser(a.typeSafeGet(Context.class),
+                    colorOptions.color(), colorOptions.terminalColor(),
+                    mode != OutputModes.NO_WRAP, false).print(node);
+        }
     }
 
     @Override
